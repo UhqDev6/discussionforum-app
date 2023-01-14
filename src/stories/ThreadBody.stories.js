f516fea6d1ae84a9acaa5bb3c5112a9c67b25bbb
M Dhiya Ulhaq <uhqdev@192.168.1.16>
2023-01-14 10:38:49 +0800

    added storybook to component testing

---
+++ src/stories/ThreadBody.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ThreadBody from '../components/atoms/ThreadBody';
+
+const stories = {
+  title: 'ThreadBody',
+  component: ThreadBody,
+};
+
+export default stories;
+
+function TemplateStory(args) {
+  return <ThreadBody {...args} />;
+}
+
+const withThreadBodyOnDetailThread = TemplateStory.bind({});
+withThreadBodyOnDetailThread.args = {
+  body: '<div>Bagaimana kabarmu? Semoga baik-baik saja ya. Sekali lagi saya ucapkan selamat datang semuanya!</div><div><br></div><div>Seperti yang sudah disampaikan sebelumnya, pada diskusi ini kamu bisa memperkenalkan diri kamu dan juga berkenalan dengan teman sekelas lainnya.</div><div><br></div><div>Berhubungan baik dengan teman sekelas dan instruktur merupakan bagian penting dari pembelajaran di kelas ini, karena mereka dapat membantu jika kamu mengalami kendala dalam mempelajari dan memahami materi.&nbsp;&nbsp;</div><div><br></div><div>Oleh karena itu, luangkanlah waktumu untuk saling mengenal dan mencairkan suasana. Membangun interaksi dengan siswa lain akan membuat pengalaman belajar kamu jauh lebih menyenangkan dan menarik.&nbsp;</div><div><br></div><div>Beberapa hal yang dapat kamu tulis pada perkenalan diri:</div><div><br></div><div>1. Siapa kamu dan dari mana kamu berasal?</div><div>2. Apa pekerjaan atau pendidikan kamu saat ini?</div><div>3. Kenapa kamu mengambil pelatihan ini? Apakah mungkin karena kamu sedang mengejar perubahan dalam karir, atau lainnya?</div>',
+  contentFull: true,
+};
+
+export { withThreadBodyOnDetailThread };
